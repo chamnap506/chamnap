623950f122ab37d85e4d4b3721202e3a01f2048c
chamnap506 <57656320+chamnap506@users.noreply.github.com>
2019-11-12 13:48:41 +0700

    Error not showing list-employee

---
+++ springmybatis/src/main/java/in/chamnap/springmybatis/controller/EmployeeController.java
@@ -0,0 +1,16 @@
+package in.chamnap.springmybatis.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller	
+
+public class EmployeeController {
+  
+	@RequestMapping ("/")
+	public String index() {
+		return "list-employees";
+	}
+	
+	
+}
